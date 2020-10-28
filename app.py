9343879239df52969a2c1f8be8bf95fef789babf
ronikhizverg <khizverg@gmail.com>
2020-10-28 22:51:38 +0200

    /

--- app.py
+++ app.py
@@ -5,56 +5,33 @@ from flask import Flask, request, render_template
 app = Flask("__name__")
 
 
-@app.route("/add")
-def addition():
-    if 'a' in request.args:
-        a = int(request.args.get('a'))
-    else:
-        a = 0
-    if 'b' in request.args:
-        b = int(request.args.get('b'))
-    else:
-        b = 0
-    return str(a) + ' + ' + str(b) + ' == ' + str(a + b) + '<br>' + "thanks" + '<br>'
-
-
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
-
-
-@app.route('/rail')
 def rail_schedule():
     if 'outformat' in request.args:
         outformat = request.args.get('outformat')
     else:
         outformat = 'html'
-        print("roni")
 
     if 'source' in request.args:
         source = request.args.get('source')
     else:
-        source = 'def'
-        print("roni")
+        source = 'Modiin'
 
     if 'destination' in request.args:
         destination = request.args.get('destination')
     else:
-        destination = 'def'
-        print("roni")
+        destination = 'Lod'
 
     if 'hour' in request.args:
         hour = request.args.get('hour')
 
     else:
-        hour = '0'
-        print("roni")
+        hour = '5'
 
     if 'minutes' in request.args:
         minutes = request.args.get('minutes')
     else:
-        minutes = '0'
-        print("roni")
+        minutes = '30'
 
     return subprocess.check_output(
         ["java", "-classpath", "/home/ronikhizverg/git/TrainSchedule/TrainSchedule/bin", "userSystem/UserMain",
